c37a56b45ac8f944af925eef37b415072c1b44bf
INO <ink.avc@gmail.com>
2025-01-17 08:11:55 +0900

    Update コンテンツを修正

--- wordpress/script/LT_post_content.sh
+++ wordpress/script/LT_post_content.sh
@@ -15,7 +15,7 @@ echo "<div class=min-vh-100 d-flex align-items-center>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>好きなことは、「偽物作り」今回ご紹介するのもWP環境の仮想化っていう意味では偽物サイト作り。</li>
+<li>好きなことは、「偽物作り」です。（自動テスト, モック, テストデータ, 仮想化）</li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list -->
 
@@ -34,14 +34,14 @@ echo "<div class=min-vh-100 d-flex align-items-center>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><strong>「クラッシュすると怒られるWPサイトを、チームで開発している人へ。」</strong></p>
+<p><strong>「1日にいくつもWPサイトを更新する / チームで開発している」人へ。</strong></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:list -->
 <ul class="wp-block-list"><!-- wp:list-item -->
 <li>開発者...<!-- wp:list -->
 <ul class="wp-block-list"><!-- wp:list-item -->
-<li>直接FTPでサーバーを触って、よくチームメンバーとバッティングする。<br>ふと、気づいたら一部のページでエラーになってる。<br>エラーを解消したと思ったら他の箇所でエラーになってる。<br>もうなんかコードを触るのが怖い！！</li>
+<li>直接FTPでサーバーを触って、よくチームメンバーとバッティングする。<br>ふと、気づいたら一部のページでエラーになってる。<br>エラーを解消したと思ったら他の箇所でエラーになってる。<br>もうなんかコードを触るのが怖い！！（ビビって色々できない）</li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list --></li>
 <!-- /wp:list-item -->
@@ -50,10 +50,6 @@ echo "<div class=min-vh-100 d-flex align-items-center>
 <li>発注者...<!-- wp:list -->
 <ul class="wp-block-list"><!-- wp:list-item -->
 <li>１日に複数のサイトを編集すると切り替えが大変。</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>納品されたコード（テーマ）を動かすのに時間がかかる。</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
@@ -68,7 +64,7 @@ echo "<div class=min-vh-100 d-flex align-items-center>
 <!-- /wp:list -->
 
 <!-- wp:paragraph -->
-<p><strong>逆に、「本番サイトは壊れても問題なくて、何よりも開発スピード重視！！作ったあと一切関与しない。」っていう人には刺さらない...かも？</strong></p>
+<p><strong>逆に、「とにかく、何よりも、スピード重視!! 作ったあとは、まいっか。」っていう人には刺さらない...かも？</strong></p>
 <!-- /wp:paragraph -->
 
 </div>
@@ -86,26 +82,30 @@ echo "<div class=min-vh-100 d-flex align-items-center>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><strong>「高精度な”偽物”を使って、安全に速く作る。」</strong></p>
+<p><strong>「高精度な”偽物”を使って、安全に速くチームで作れる。」</strong></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:list -->
 <ul class="wp-block-list"><!-- wp:list-item -->
-<li>安全性: 手元で動かすのは偽物なので、開発中にサイトを壊しても大丈夫！</li>
+<li>安全で: 手元で動かすのは偽物なので、開発中にサイトを壊しても大丈夫！</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>柔軟性: 偽物を見ながら会話できるので外注さんとのやりとりが速い。</li>
+<li>柔軟で: プラグインやWPのアップデート時の検証に強い。</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>可読性: コードベースになるのでGitも使えるようになってPRレビューできるようになる。</li>
+<li>可読で: コードベースになるのでGitも使えるようになってPRレビューできるようになる。</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>共有性: コマンドをいくつか打つと全員が同じ画面をすぐに見れるので共有が楽！</li>
+<li>共有な: コマンドをいくつか打つと全員が同じWPサイトをすぐに見れる。</li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p><strong>とりあえず、「顎関節症」になることはなくなるはず！</strong></p>
+<!-- /wp:paragraph -->
 
 </div>
 </div>
@@ -156,33 +156,24 @@ $ cp .env.example .env
 $ docker compose up -d</code></pre>
 <!-- /wp:code -->
 
-<!-- wp:paragraph -->
-<p>Localhostにアクセス（言語選択画面が表示される）</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:list -->
-<ol class=wp-block-list></ol>
-<!-- /wp:list -->
+<!-- wp:code -->
+<pre class="wp-block-code"><code># Localhostにアクセス（言語選択画面が表示される）
+# http://localhost:8002</code></pre>
+<!-- /wp:code -->
 
 <!-- wp:code -->
-<pre class="wp-block-code"><code>$ docker compose exec wordpress bash /script/setup.sh</code></pre>
+<pre class="wp-block-code"><code>$ docker compose exec wordpress bash /script/setup.sh
+# WPの色々設定用のスクリプト（WP-CLI）</code></pre>
+<!-- /wp:code -->
+
+<!-- wp:code -->
+<pre class="wp-block-code"><code># Localhostにアクセス（設定後の画面が表示される）
+# http://localhost:8002</code></pre>
 <!-- /wp:code -->
 
 <!-- wp:paragraph -->
-<p>Localhostにアクセス（設定後の画面が表示される）</p>
+<p>詳細は、Githubでリポジトリを公開しています。<br><a href="https://github.com/playground2411/docker-wordpress-lt-ver" target="_blank" rel="noreferrer" noopener="">https://github.com/playground2411/docker-wordpress-lt-ver</a></p>
 <!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>詳細は、Githubでリポジトリを公開しています。<br><a href=https://github.com/playground2411/docker-wordpress-lt-ver target=_blank rel=noreferrer noopener>https://github.com/playground2411/docker-wordpress-lt-ver</a></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:list -->
-<ol class=wp-block-list></ol>
-<!-- /wp:list -->
-
-<!-- wp:list -->
-<ol class=wp-block-list></ol>
-<!-- /wp:list -->
 
 <!-- wp:paragraph -->
 <p>必要なものは「Docker Desktop」だけ。</p>
@@ -190,27 +181,6 @@ $ docker compose up -d</code></pre>
 
 </div>
 </div>
-
-<!-- wp:paragraph -->
-<p></p>
-<!-- /wp:paragraph -->
-
-<div class=min-vh-100 d-flex align-items-center>
-<div>
-
-<!-- wp:heading -->
-<h2 class="wp-block-heading">今後の課題</h2>
-<!-- /wp:heading -->
-
-<!-- wp:list {"ordered":true} -->
-<ol class="wp-block-list"><!-- wp:list-item -->
-<li>学習コスト（Docker, WP-CLI）</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>Dockerのメンテナンス・コスト</li>
-<!-- /wp:list-item --></ol>
-<!-- /wp:list -->
 
 </div>
 </div>
@@ -228,11 +198,11 @@ $ docker compose up -d</code></pre>
 
 <!-- wp:list -->
 <ul class="wp-block-list"><!-- wp:list-item -->
-<li>デプロイはGithubCIで自動化しています。</li>
+<li>デプロイはGithubCIで自動化しています。（リリースタグ作成時に自動デプロイ）</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>実はnodeコンテナを使ってsassを導入しています。</li>
+<li>実はnodeコンテナを使ってsassを導入しています。（scss変更で自動ビルド）</li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list -->
 
