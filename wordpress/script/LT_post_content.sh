8455388811fecaaacd989b21433511901dff5299
INO <ink.avc@gmail.com>
2025-01-17 19:56:21 +0900

    Update メッセージをシャープにした

--- wordpress/script/LT_post_content.sh
+++ wordpress/script/LT_post_content.sh
@@ -16,6 +16,14 @@ echo "<div class=min-vh-100 d-flex align-items-center>
 
 <!-- wp:list-item -->
 <li>好きなことは、「偽物作り」です。（自動テスト, モック, テストデータ, 仮想化）</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>週末は個人参加型のフットサルに参加してます。</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>平日は家でAPEXでランクマやってます。</li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list -->
 
@@ -34,38 +42,26 @@ echo "<div class=min-vh-100 d-flex align-items-center>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><strong>「1日にいくつもWPサイトを更新する / チームで開発している」人へ。</strong></p>
+<p><strong>「WPサイト開発ってなんか怖いなあ…」と思ったことのある人へ。</strong></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:list -->
 <ul class="wp-block-list"><!-- wp:list-item -->
-<li>開発者...<!-- wp:list -->
-<ul class="wp-block-list"><!-- wp:list-item -->
-<li>直接FTPでサーバーを触って、よくチームメンバーとバッティングする。<br>ふと、気づいたら一部のページでエラーになってる。<br>エラーを解消したと思ったら他の箇所でエラーになってる。<br>もうなんかコードを触るのが怖い！！（ビビって色々できない）</li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></li>
+<li>FTPで直接サーバーを触ってる？</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>発注者...<!-- wp:list -->
-<ul class="wp-block-list"><!-- wp:list-item -->
-<li>１日に複数のサイトを編集すると切り替えが大変。</li>
+<li>他メンバーと作業がバッティングしてサイトがおかしくなる？</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>投稿や固定ページのデータをいちいち作るのが億劫。</li>
+<li>エラーを１つ解消したら他でエラーになる？</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>WordpressでもGitを使いたい！</li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></li>
+<li>毎日投稿や固定ページのデータをいちいち作るのが億劫？</li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list -->
-
-<!-- wp:paragraph -->
-<p><strong>逆に、「とにかく、何よりも、スピード重視!! 作ったあとは、まいっか。」っていう人には刺さらない...かも？</strong></p>
-<!-- /wp:paragraph -->
 
 </div>
 </div>
@@ -82,12 +78,12 @@ echo "<div class=min-vh-100 d-flex align-items-center>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><strong>「高精度な”偽物”を使って、安全に速くチームで作れる。」</strong></p>
+<p><strong>「高精度な”偽物”を使うと、安全に速くチームで作れるようになる。」</strong></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:list -->
 <ul class="wp-block-list"><!-- wp:list-item -->
-<li>安全で: 手元で動かすのは偽物なので、開発中にサイトを壊しても大丈夫！</li>
+<li>安全で: 手元で動かすのは偽物なので、開発中にサイトを壊しても大丈夫！<br><strong>（セーブポイントとリセットボタンがあるみたい！）</strong></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
@@ -95,7 +91,7 @@ echo "<div class=min-vh-100 d-flex align-items-center>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>可読で: コードベースになるのでGitも使えるようになってPRレビューできるようになる。</li>
+<li>可読で: コードレビューができるので責任回避できる！</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
@@ -104,7 +100,7 @@ echo "<div class=min-vh-100 d-flex align-items-center>
 <!-- /wp:list -->
 
 <!-- wp:paragraph -->
-<p><strong>とりあえず、「顎関節症」になることはなくなるはず！</strong></p>
+<p><strong>とりあえず、「顎関節症」になることはなくなるはず！</strong>（共有環境での作業より）</p>
 <!-- /wp:paragraph -->
 
 </div>
@@ -129,16 +125,13 @@ echo "<div class=min-vh-100 d-flex align-items-center>
 <p>「コードとコマンドで作る」っていういにしえの開発手法を現代風にアレンジして見ました。</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:list -->
-<ol class=wp-block-list>
-<!-- wp:list-item -->
-<li>Dockerを使って、偽物のWPサイトを作る。</li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li>WP-CLIを使って、偽物サイトにデータを流し込む。</li>
-<!-- /wp:list-item -->
-</ol>
-<!-- /wp:list -->
+<!-- wp:paragraph -->
+<p>（でも受託開発だと... まだまだコードベースで作ってる...よね？）</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
 
 </div>
 </div>
@@ -156,31 +149,43 @@ $ cp .env.example .env
 $ docker compose up -d</code></pre>
 <!-- /wp:code -->
 
-<!-- wp:code -->
-<pre class="wp-block-code"><code># Localhostにアクセス（言語選択画面が表示される）
-# http://localhost:8002</code></pre>
-<!-- /wp:code -->
+<!-- wp:paragraph -->
+<p>Localhostにアクセス（言語選択画面が表示される）<br><a href="http://localhost:8002" target="_blank" rel="noreferrer noopener">http://localhost:8002</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>-- ここまでで、Dockerを使ったWPサイト構築は完了 --</p>
+<!-- /wp:paragraph -->
 
 <!-- wp:code -->
 <pre class="wp-block-code"><code>$ docker compose exec wordpress bash /script/setup.sh
 # WPの色々設定用のスクリプト（WP-CLI）</code></pre>
 <!-- /wp:code -->
 
-<!-- wp:code -->
-<pre class="wp-block-code"><code># Localhostにアクセス（設定後の画面が表示される）
-# http://localhost:8002</code></pre>
-<!-- /wp:code -->
-
 <!-- wp:paragraph -->
-<p>詳細は、Githubでリポジトリを公開しています。<br><a href="https://github.com/playground2411/docker-wordpress-lt-ver" target="_blank" rel="noreferrer" noopener="">https://github.com/playground2411/docker-wordpress-lt-ver</a></p>
+<p>Localhostにアクセス（設定後の画面が表示される）<br><a href="http://localhost:8002" target="_blank" rel="noreferrer noopener">http://localhost:8002</a></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p>必要なものは「Docker Desktop」だけ。</p>
+<p><strong>必要なものは「Docker Desktop」だけ。</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
 <!-- /wp:paragraph -->
 
 </div>
 </div>
+<div class=min-vh-100 d-flex align-items-center>
+<div>
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Githubで公開してるよ</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>上の手順も含めて詳細とコードを、Githubでリポジトリを公開しています。<br>上と同じように5つコマンドを打てば今見ているサイトをお手元で再現できます。<br><a noopener="" href="https://github.com/playground2411/docker-wordpress-lt-ver" target="_blank" rel="noreferrer">https://github.com/playground2411/docker-wordpress-lt-ver</a></p>
+<!-- /wp:paragraph -->
 
 </div>
 </div>
@@ -193,16 +198,59 @@ $ docker compose up -d</code></pre>
 <div>
 
 <!-- wp:heading -->
-<h2 class="wp-block-heading">その他</h2>
+<h2 class="wp-block-heading">ぜひお話ししましょう！</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>こういう話題が大好きです</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li>開発環境のあれこれ</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>デプロイ自動化</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>モダンJSなフロントエンドを導入</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>WordpressのAPI化</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Headless CMS</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+</div>
+</div>
+<div class=min-vh-100 d-flex align-items-center>
+<div>
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">その他個人的なこだわり（時間が余ったら）</h2>
 <!-- /wp:heading -->
 
 <!-- wp:list -->
 <ul class="wp-block-list"><!-- wp:list-item -->
-<li>デプロイはGithubCIで自動化しています。（リリースタグ作成時に自動デプロイ）</li>
+<li>デプロイはGithubCIで自動化しています。<br>（ファイルをドラッグしてデプロイするなんて怖すぎてできない）</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>実はnodeコンテナを使ってsassを導入しています。（scss変更で自動ビルド）</li>
+<li>実はnodeコンテナを使ってsassを導入しています。<br>（BootstrapをSASSで編集したかった...）</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>XserverVPSをメインで使ってます<br>（持ってるMacが非力すぎてDockerが動かなかった）</li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list -->
 
