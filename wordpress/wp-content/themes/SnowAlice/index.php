1bd23f39f6924ac65cbc216378ddcdd7bf8898be
INO <ink.avc@gmail.com>
2025-01-24 12:30:25 +0900

    Update fontとサムネイルを追加

--- wordpress/wp-content/themes/SnowAlice/index.php
+++ wordpress/wp-content/themes/SnowAlice/index.php
@@ -1,11 +1,15 @@
 <?php get_header(); ?>
 <section>
-  <div class="container-fluid">
+  <div class="container-fluid"
+  style="background: linear-gradient(rgba(0, 0, 0, .6),  rgba(0, 0, 0, .6)), url('<?php echo get_template_directory_uri() . '/assets/images/thumbnail.png';?>');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;">
     <div class="row">
       <div class="col-12 col-lg-10 offset-lg-1">
-        <div class="d-flex align-items-center min-vh-100">
-          <div class="mx-auto w-100 text-center">
-            <h2>2025/1/25のLT資料</h2>
+        <div class="d-flex align-items-center min-vh-80">
+          <div class="mx-auto w-100 text-center text-white">
+            <h2 class="pattaya-regular">2025/1/25 LT document.</h2>
           </div>
         </div>
       </div>
